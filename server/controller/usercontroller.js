e604b929ec16f6db95f75baea41b2e7104d1cea2
Your Name <you@example.com>
2023-06-22 15:37:10 +0530

    signin api

--- server/controller/usercontroller.js
+++ server/controller/usercontroller.js
@@ -1,15 +1,32 @@
 const users = require("../modles/usermodel")
 
 exports.postuser=(req,res)=>{
-    users.create({
-        name:req.body.name,
-        email:req.body.email,
-        password:req.body.password
-    }).then(()=>{
-        res.json("sucsees")
-    }).catch((err)=>{
-        res.json("user already exist")
-    })
+    if(req.body.name){
+        users.create({
+            name:req.body.name,
+            email:req.body.email,
+            password:req.body.password
+        }).then(()=>{
+            res.json("sucsees")
+        }).catch((err)=>{
+            res.json("user already exist")
+        })
+    }
+    else{
+        users.findAll({
+            where:{
+                email:req.body.email
+            }
+        }).then((result)=>{
+             if(result[0].password===req.body.password){
+                return res.json("login sucess")
+             }
+             else{
+                return res.json("password does not match")
+             }
+        })
+    }
+   
 
 }
 exports.getuser=(req,res)=>{
